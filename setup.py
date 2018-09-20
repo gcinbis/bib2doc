4817512dd73626e1bc8d18c83abf96a38df42b2b
Gokberk Cinbis <gokberkcinbis@gmail.com>
2018-09-21 00:35:18 +0300

    tool rename, doc improvements

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name='bib2doc',    
-    version='0.1.4',                          
-    scripts=['bib2html']                  
+    version='0.1.5',                          
+    scripts=['bib2doc','bib2html']                  
 )
 
 # Use "python setup.py sdist upload" to update pypi package.
