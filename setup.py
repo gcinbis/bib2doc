c90f31414d2083bff4b2a6c9a3fad34eac095e6d
Gokberk Cinbis <gokberkcinbis@gmail.com>
2019-01-31 08:29:22 +0300

    Version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bib2doc',    
-    version='0.1.6',                          
+    version='0.1.7',                          
     scripts=['bib2doc']                  
 )
 
