815c049f0a32340c0bc67a8736fbd2a1bddbbd19
Gokberk Cinbis <gokberkcinbis@gmail.com>
2019-03-07 01:42:07 +0300

    New version with auto duplicate remover.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bib2doc',    
-    version='0.1.8',                          
+    version='0.1.9',                          
     scripts=['bib2doc']                  
 )
 
